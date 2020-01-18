b06b6abe0afca1839bac719a468b94a6775fde9f
burhan <burskapasi@hotmail.co.uk>
2020-01-18 16:00:27 +0000

    project Pages with button

--- app/src/main/java/com/example/climatechange/MainActivity.java
+++ app/src/main/java/com/example/climatechange/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.climatechange;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button news;
+    private Button game;
+    private Button profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        news = findViewById(R.id.buttonNews);
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityNews();
+            }
+        });
+
+        game = findViewById(R.id.buttonGame);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityGame();
+            }
+        });
+
+        profile = findViewById(R.id.buttonProfile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityProfile();
+            }
+        });
+    }
+    public void openActivityNews(){
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
+    }
+    public void openActivityGame(){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+    public void openActivityProfile(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
